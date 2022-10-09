e6c19dd4e3b2d1fc68d3dfb355169de5e4e746d6
cstarshine <cstarshiny@gmail.com>
2022-10-09 20:53:52 +0900

    first twitter request

--- index.js
+++ index.js
@@ -1,142 +1,148 @@
-var apiKey;
-var apiSecretKey;
-var bearerToken;
-
-var xlsx = xlsxController();
-var ajax = ajaxController();
-
-const xlsxController = () => {
-    var matchRow;
-
-    let tweet;
-
-    $(document).ready(function () {
-      matchRow = $(".match_row").html();
-    });
-
-    $(".add_row").click(function () {
-      $(this).before("<tr class='match_row'>" + matchRow + "</tr>");
-    });
-
-    $(".del_row").click(function () {
-      $(this).parent().parent().remove();
-    });
-
-    function makeNameArray() {
-      var nameArray = new Array();
-
-      $(".match_row").each((key, val) => {
-        var nameObject = new Object();
-        nameObject[$(val).find(".user_id").val()] = $(val).find(".user_name").val();
-
-        nameArray.push(nameObject);
-      });
-
-      return nameArray;
-    }
-
-    function makeDataArray(nameArray) {
-      var dataArray = new Array();
-
-      tweet = $.parseJSON(tweet);
-
-      $.each(tweet, (key, tweetData) => {
-        $.each(tweetData, (t, val) => {
-          var data = new Object();
-          data.date = new Date(val.created_at).toISOString();
-          if (val.entities.user_mentions[0] != undefined || val.entities.user_mentions[0] != undefined) {
-            data.mention_name = val.entities.user_mentions[0].name;
-          } else {
-            $.each(nameArray, (index, nameData) => {
-              $.each(nameData, (user_id, user_name) => {
-                var regex = new RegExp(user_id);
-                if (val.full_text.match(regex) != null) {
-                  data.mention_name = user_name;
-                }
-              })
-            });
-          }
-          data.text = val.full_text.replace(/^@\w*\s/, "");
-
-          dataArray.push(data);
+var xlsxController = {
+    matchRow: "",
+    initEvent: function () {
+        $(".add_row").click(function () {
+            $(this).before("<tr class='match_row'>" + this.matchRow + "</tr>");
         });
-      });
+        $(".del_row").click(function () {
+            $(this).parent().parent().remove();
+        });
+    },
+    initFunc: function () {
+        function makeNameArray() {
+            var nameArray = new Array();
 
-      dataArray.sort((a, b) => {
-        return new Date(a.date) - new Date(b.date);
-      });
-      return dataArray;
-    }
+            $(".match_row").each((key, val) => {
+                var nameObject = new Object();
+                nameObject[$(val).find(".user_id").val()] = $(val).find(".user_name").val();
 
-    $("#tweet_file").on("change", function(){
-      let file = $("#tweet_file")[0].files[0];
-      let fr = new FileReader();
-      fr.onload = () => {
-        var jsonStr = JSON.stringify(fr.result);
-        var jsonData = JSON.parse(jsonStr);
-        tweet = jsonData;
-      };
-      fr.readAsText(file);
-    })
+                nameArray.push(nameObject);
+            });
 
-    function exportExcel() {
-      let nameArray = makeNameArray();
+            return nameArray;
+        }
+        function makeDataArray(nameArray) {
+            var dataArray = new Array();
+            if (typeof tweet == undefined) {
+                return false;
+            }
+            tweet = $.parseJSON(tweet);
 
-      let dataArray = makeDataArray(nameArray);
+            $.each(tweet, (key, tweetData) => {
+                $.each(tweetData, (t, val) => {
+                    var data = new Object();
+                    data.date = new Date(val.created_at).toISOString();
+                    if (val.entities.user_mentions[0] != undefined || val.entities.user_mentions[0] != undefined) {
+                        data.mention_name = val.entities.user_mentions[0].name;
+                    } else {
+                        $.each(nameArray, (index, nameData) => {
+                            $.each(nameData, (user_id, user_name) => {
+                                var regex = new RegExp(user_id);
+                                if (val.full_text.match(regex) != null) {
+                                    data.mention_name = user_name;
+                                }
+                            })
+                        });
+                    }
+                    data.text = val.full_text.replace(/^@\w*\s/, "");
 
-      var workBook = XLSX.utils.book_new();
+                    dataArray.push(data);
+                });
+            });
 
-      var newWorkSheet = XLSX.utils.json_to_sheet(dataArray);
+            dataArray.sort((a, b) => {
+                return new Date(a.date) - new Date(b.date);
+            });
+            return dataArray;
+        }
+        function exportExcel() {
+            let nameArray = makeNameArray();
 
-      XLSX.utils.book_append_sheet(workBook, newWorkSheet, "백업");
+            let dataArray = makeDataArray(nameArray);
 
-      var workBookOut = XLSX.write(workBook, { bookType: 'xlsx', type: 'binary' });
+            var workBook = XLSX.utils.book_new();
 
-      saveAs(new Blob([s2ab(workBookOut)], { type: "application/octet-stream" }), "백업.xlsx");
-    }
+            var newWorkSheet = XLSX.utils.json_to_sheet(dataArray);
 
-    function s2ab(s) {
-      var buf = new ArrayBuffer(s.length);
-      var view = new Uint8Array(buf);
-      for (var i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xFF;
-      return buf;
+            XLSX.utils.book_append_sheet(workBook, newWorkSheet, "백업");
+
+            var workBookOut = XLSX.write(workBook, { bookType: 'xlsx', type: 'binary' });
+
+            saveAs(new Blob([s2ab(workBookOut)], { type: "application/octet-stream" }), "백업.xlsx");
+        }
+        function s2ab(s) {
+            var buf = new ArrayBuffer(s.length);
+            var view = new Uint8Array(buf);
+            for (var i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xFF;
+            return buf;
+        }
+        $("#tweet_file").on("change", function () {
+            let file = $("#tweet_file")[0].files[0];
+            let fr = new FileReader();
+            fr.onload = () => {
+                var jsonStr = JSON.stringify(fr.result);
+                var jsonData = JSON.parse(jsonStr);
+                tweet = jsonData;
+            };
+            fr.readAsText(file);
+        });
+    },
+    run: function () {
+        this.matchRow = $(".match_row").html();
+        this.initEvent();
+        this.initFunc();
     }
 }
 
-const ajaxController = () => {
-    let protocolType;
-    let url;
-    let data;
-    let headerList = {};
-
-    function setProtocol(protocolType) {
+var ajaxController = {
+    protocolType: "",
+    url: "",
+    data: "",
+    dataType: "",
+    headerList: {},
+    setProtocol: function (protocolType) {
         this.protocolType = protocolType;
-    }
-
-    function setUrl(url) {
-        this.url = url; 
-    }
-
-    function setData(data){
+    },
+    setUrl: function (url) {
+        this.url = url;
+    },
+    setData: function (data, dataType) {
         this.data = data;
-    }
-
-    function execute() {
+    },
+    addHeaderList: function (headerType, headerValue) {
+        this.headerList[headerType] = headerValue;
+    },
+    execute: function() {
+        var self = this;
         $.ajax({
-            type: this.protocolType,
-            url: this.url,
-            data: this.data,
-            beforeSend: function(xhr) {
-                $.each(this.headerList, (key, val) => {
+            type: self.protocolType,
+            url: self.url,
+            data: self.data,
+            dataType: self.dataType,
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+                $.each(self.headerList, (key, val) => {
                     xhr.setRequestHeader(key, val);
                 });
             },
             success: function (response) {
                 console.log(response);
             },
-            error: function(e) {
+            error: function (e) {
                 console.log(error);
             }
         });
     }
 }
+
+var xlsx = xlsxController;
+var ajax = ajaxController;
+
+$("#devExportExcel").click(function () {
+    xlsx.exportExcel();
+})
+
+ajax.setProtocol("GET");
+ajax.addHeaderList("Authorization", "Bearer 3246597894-pdkBRZlE7D17fEg1lsKxv9JCulnh7Aq1x3hoyOV")
+ajax.setUrl("https://api.twitter.com/2/users/by/username/starshine");
+ajax.execute();
